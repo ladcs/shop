2e7ab8e87978269cc581bdf3b2271d4e0dfc6008
ladcs <hpladcs@gmail.com>
2023-05-14 17:50:29 -0300

    feat: README.md backend

--- backend/src/model/connection.ts
+++ backend/src/model/connection.ts
@@ -4,7 +4,7 @@ import * as mysql from 'mysql2/promise';
 dotenv.config();
 
 const mysqlUser = process.env.MYSQL_USER ?? "root";
-const mysqlPass = process.env.MYSQL_PASSWORD ?? "root";
+const mysqlPass = process.env.MYSQL_PASSWORD ?? "123456";
 const mysqlPort = process.env.PORT ? parseInt(process.env.PORT, 10) : 3306;
 
 const connection = mysql.createPool({
