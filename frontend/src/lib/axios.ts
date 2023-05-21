6ae23f10c135257d219ab3e436e34b867b8ec36d
ladcs <hpladcs@gmail.com>
2023-05-21 16:01:16 -0300

    feat: frontend in docker

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const host = process.env.NEXT_PUBLIC_HOST ?? 'backend';
+const backendPort = process.env.NEXT_PUBLIC_BACKEND_PORT ? parseInt(process.env.NEXT_PUBLIC_BACKEND_PORT, 10) : 3001;
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: `http://${host}:${backendPort}/`,
 });
